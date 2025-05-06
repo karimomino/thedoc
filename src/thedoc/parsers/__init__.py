347f2269d47c3cc98fa3ef0f35226313fd282295
Karim Amin <karim.amin@adek.gov.ae>
2025-05-06 18:32:55 +0400

    refactor: clean up dotnet parser implementation and tests

--- src/thedoc/parsers/__init__.py
+++ src/thedoc/parsers/__init__.py
@@ -2,5 +2,6 @@
 
 from .base import BaseParser, DocItem
 from .python import PythonParser
+from .dotnet_parser import DotNetParser
 
-__all__ = ['BaseParser', 'DocItem', 'PythonParser'] 
+__all__ = ['BaseParser', 'DocItem', 'PythonParser', 'DotNetParser'] 
